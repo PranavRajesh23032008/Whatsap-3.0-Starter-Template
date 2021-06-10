4dfeda001a830acea3b0e147a876b55062456fb3
PranavRajesh23032008 <pranv@rckr.com>
2021-06-11 00:53:45 +0530

    Added Custom Colors to the tailwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        whatsapp_green: {
+          light: "#24CC63",
+          DEFAULT: "#11887A",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
